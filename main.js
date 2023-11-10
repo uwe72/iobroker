07f18a5f530dfbc829108c7e8a0465232ea40ba1
Uwe Clement <uwe.clement@exxcellent.de>
2023-11-10 18:03:17 +0100

    eee

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-console.log("main h66 ello!");
-console.log("main h66 ello!22223");
+uwe();
+
+function uwe() {
+    console.log("This is inside uwe()");
+    console.log("main h66 ello!");
+    console.log("main h66 ello!22223");
+}
